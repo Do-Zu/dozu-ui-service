301e5d85d56d0a82593cb7b17e771f50474ef5ce
vinhpm <cinepa.org@gmail.com>
2025-01-30 23:51:02 +0700

    test(pipeline): test choice pipeline

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,7 @@ import { NextPage } from 'next';
 const HomePage: NextPage = () => {
   return (
     <>
-      <div>Testing Pipeline</div>
+      <div>Testing Pipeline Staging feature/merge</div>
       <Button>shacdn</Button>
       <DataTableDemo />
     </>
