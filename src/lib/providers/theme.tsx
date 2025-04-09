759c2dd78ad006dc03256e7559e0fca8af064c27
thai-hoang-03 <buithaihoang03@gmail.com>
2025-04-09 20:51:41 +0700

    check lint

--- src/lib/providers/theme.tsx
+++ src/lib/providers/theme.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { ReactNode } from 'react';
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
+    const     testLint =     123
+
   return (
     <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
