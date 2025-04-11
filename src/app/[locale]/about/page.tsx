2397d35ca1032b62ec801947c3dab7949edffb76
vinhpm <cinepa.org@gmail.com>
2025-04-11 15:53:02 +0700

    feat(pipeline): stage for pipeline v1 vercel

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import React from 'react';
-import Profile from './_components/Profile';  // Import Profile component
+import Profile from './_components/Profile'; // Import Profile component
 
 const AboutPage: React.FC = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Page CI</h1>
       <Profile name="John Doe" age={30} bio="A passionate developer!" />
     </div>
   );
