fed5bae79fe681da8b118b22b85b11474178b775
vinhpm <cinepa.org@gmail.com>
2025-04-20 11:44:17 +0700

    feat(generate): update comment review

--- src/app/[locale]/generate/_components/CardImport.tsx
+++ src/app/[locale]/generate/_components/CardImport.tsx
@@ -36,6 +36,7 @@ import { reduceTokenInput } from '../helper/reduceTokenInput';
 import ContentDetailView from '../detail-extract/components/ContentDetailView';
 import usePost from '@/hooks/usePost';
 import ProcessGenerate from './ProcessGenerate';
+import Final from './steps/Final';
 
 interface CardImportProps {
   onOpenChange?: (open: boolean) => void;
@@ -208,55 +209,7 @@ const CardImport: React.FC<CardImportProps> = ({
       case 2:
         return <ContentDetailView />;
       case 3:
-        <></>;
-      // return (
-      //   <div className="space-y-6">
-      //     <div className=" p-4 rounded-lg border border-gray-200">
-      //       <h3 className="font-medium  mb-3">Content Preview</h3>
-      //       <div className=" border  rounded-md p-3 max-h-[200px] overflow-y-auto">
-      //         {importMethod === 'text' ? (
-      //           <p className="text-sm ">{textContent}</p>
-      //         ) : (
-      //           <ul className="space-y-1">
-      //             {files.map((file, index) => (
-      //               <li key={index} className="text-sm flex items-center">
-      //                 <File className="h-4 w-4 mr-2" />
-      //                 {file.name}
-      //               </li>
-      //             ))}
-      //           </ul>
-      //         )}
-      //       </div>
-      //     </div>
-
-      //     <div className="space-y-3">
-      //       <h3 className="font-medium ">
-      //         Learning Method: <span className="capitalize">{selectedMethod}</span>
-      //       </h3>
-
-      //       <div className="grid grid-cols-2 gap-4">
-      //         <div className="space-y-2">
-      //           <label className="text-sm font-medium ">Title</label>
-      //           <Input
-      //             placeholder="Enter a title for your content"
-      //             defaultValue={
-      //               files.length > 0 ? files[0].name.split('.')[0] : 'New Learning Content'
-      //             }
-      //           />
-      //         </div>
-      //         <div className="space-y-2">
-      //           <label className="text-sm font-medium ">Category</label>
-      //           <Input placeholder="Optional category" />
-      //         </div>
-      //       </div>
-
-      //       <div className="space-y-2">
-      //         <label className="text-sm font-medium ">Description</label>
-      //         <Textarea placeholder="Add a description (optional)" className="min-h-[80px]" />
-      //       </div>
-      //     </div>
-      //   </div>
-      // );
+        return <Final />;
 
       default:
         return null;
