26a53f2163a37185467011704b42d6fea2bda615
vinhpm <cinepa.org@gmail.com>
2025-04-26 19:04:40 +0700

    fix(core): fix check possible null

--- src/components/toolbar/LanguageSwitcher.tsx
+++ src/components/toolbar/LanguageSwitcher.tsx
@@ -9,20 +9,23 @@ const LanguageSwitcher = () => {
   const [isPending, startTransition] = useTransition();
 
   const changeLocale = (locale: string) => {
-    const segments = pathname.split('/');
-    segments[1] = locale; // Giả sử locale là phần đầu tiên sau "/"
-    const newPath = segments.join('/');
+    const segments = pathname?.split('/');
+    // Ensure the first segment is the locale
+    if (segments && segments[1]) {
+      segments[1] = locale;
+      const newPath = segments.join('/');
 
-    startTransition(() => {
-      router.push(newPath);
-    });
+      startTransition(() => {
+        router.push(newPath);
+      });
+    }
   };
 
   return (
     <select
       onChange={(e) => changeLocale(e.target.value)}
       className="border rounded px-2 py-1 bg-background text-foreground"
-      defaultValue={pathname.split('/')[1]}
+      defaultValue={pathname?.split('/')[1]}
     >
       <option value="en">English</option>
       <option value="vi">Tiếng Việt</option>
