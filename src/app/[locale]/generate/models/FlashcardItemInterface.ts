3bcc4be0ef1575005720b5eb2d458b1d4542a8c4
vinhpm <cinepa.org@gmail.com>
2025-04-28 22:45:35 +0700

    feat(generate): implement gen models interface for API end point

---
+++ src/app/[locale]/generate/models/FlashcardItemInterface.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type FlashcardItemInterface = {
+    /**
+     * Flashcard question or term.
+     */
+    'q': string;
+    /**
+     * Flashcard answer or definition.
+     */
+    'a': string;
+};
+
---
+++ src/app/[locale]/generate/models/GenerateContentRequestInterface.ts
@@ -0,0 +1,19 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type GenerateContentRequestInterface = {
+    /**
+     * The content to generate flashcards or quizzes from.
+     */
+    content: string;
+    /**
+     * The type of content to generate (e.g., 'flashcard', 'quiz').
+     */
+    type: string;
+    /**
+     * Optional method parameter for specifying generation behavior.
+     */
+    method?: string;
+};
+
