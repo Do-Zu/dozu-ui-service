8407107f02d017705cff9ee08272f1daf97c7f50
vinhpm <cinepa.org@gmail.com>
2025-05-26 21:04:08 +0700

    feat(core): + add message for login + margin generate component + define constant path name router

---
+++ src/utils/constants/routes.ts
@@ -0,0 +1,11 @@
+export const ROUTES = Object.freeze({
+  HOME: '/',
+  LOGIN: '/auth/login',
+  REGISTER: '/auth/register',
+  GENERATE: '/generate',
+  ONBOARDING: '/onboarding',
+  FLASHCARD: '/flashcard',
+  FLASHCARD_CREATE: '/flashcard/create',
+  FLASHCARD_EDIT: '/flashcard/edit/:id',
+  FLASHCARD_VIEW: '/flashcard/view/:id',
+});
