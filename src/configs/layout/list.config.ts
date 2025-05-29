dc90b9cd41857cd46fe06f3ae1fcfd1b281498ca
vinhpm <cinepa.org@gmail.com>
2025-05-29 17:12:19 +0700

    refactor(core): update mechanism for layout config

---
+++ src/configs/layout/list.config.ts
@@ -0,0 +1,23 @@
+import { PageLayoutConfig } from './types';
+
+// More specific patterns should come before more general ones.
+export const layoutConfigs: PageLayoutConfig[] = [
+  {
+    pathPattern: '/${locale}/welcome',
+    isDisplayHeader: false,
+    isDisplayFooter: false,
+    isDisplaySidebar: false,
+  },
+  {
+    pathPattern: '/${locale}/auth/*',
+    isDisplayHeader: false,
+    isDisplayFooter: false,
+    isDisplaySidebar: false,
+  },
+  {
+    pathPattern: '/${locale}',
+    isDisplayHeader: false,
+    isDisplayFooter: false,
+    isDisplaySidebar: false,
+  },
+];
