e256b7260706916985aee44ea956fb8fe958f587
vinhpm <cinepa.org@gmail.com>
2025-06-02 11:02:28 +0700

    chore(core): clean up

--- src/app/[locale]/generate/page.tsx
+++ src/app/[locale]/generate/page.tsx
@@ -4,29 +4,20 @@ import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
 import { Loader2 } from 'lucide-react';
 import { withAuth } from '@/hoc/withAuth';
+import CardImport from './components/CardImport';
 
 // Dynamically import CardImport with no SSR
-const CardImport = dynamic(() => import('./components/CardImport'), {
-  ssr: false,
-  loading: () => (
-    <div className="flex justify-center items-center h-screen">
-      <Loader2 className="h-8 w-8 animate-spin" />
-    </div>
-  ),
-});
+// const CardImport = dynamic(() => import('./components/CardImport'), {
+//   ssr: false,
+//   loading: () => (
+//     <div className="flex justify-center items-center h-screen">
+//       <Loader2 className="h-8 w-8 animate-spin" />
+//     </div>
+//   ),
+// });
 
 const AuthComponent = withAuth(CardImport);
 
 export default function HomePage() {
-  return (
-    <Suspense
-      fallback={
-        <div className="h-screen flex items-center justify-center">
-          <Loader2 className="animate-spin h-8 w-8" />
-        </div>
-      }
-    >
-      <AuthComponent />
-    </Suspense>
-  );
+  return <AuthComponent />;
 }
