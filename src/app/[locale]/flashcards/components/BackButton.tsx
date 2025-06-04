08491b59650243baf3f7dc247e33938d00f1503e
KyAnh1126 <anh261104@gmail.com>
2025-06-04 15:00:44 +0700

    update code that apply format prettier

--- src/app/[locale]/flashcards/components/BackButton.tsx
+++ src/app/[locale]/flashcards/components/BackButton.tsx
@@ -1,24 +1,24 @@
-import { Button } from "@/components/ui/button";
-import { ArrowBigLeft } from "lucide-react";
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
-import { useRouter } from "next/navigation";
+import { Button } from '@/components/ui/button';
+import { ArrowBigLeft } from 'lucide-react';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { useRouter } from 'next/navigation';
 
 interface Props {
-    router?: AppRouterInstance
+  router?: AppRouterInstance;
 }
 
 export default function BackButton(props: Props) {
-    let { router } = props;
-    if(!router) router = useRouter();
+  let { router } = props;
+  if (!router) router = useRouter();
 
-    function handleClickBack() {
-        router?.back();
-    }
+  function handleClickBack() {
+    router?.back();
+  }
 
-    return (
-        <Button onClick={handleClickBack} className="flex flex-row items-center">
-            <ArrowBigLeft/>
-            <div className="text-base" >Back</div>
-        </Button>
-    )
+  return (
+    <Button onClick={handleClickBack} className="flex flex-row items-center">
+      <ArrowBigLeft />
+      <div className="text-base">Back</div>
+    </Button>
+  );
 }
