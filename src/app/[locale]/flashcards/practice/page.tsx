6badbdf98ee51fb7ae74e717ff14a50b669f6601
KyAnh1126 <anh261104@gmail.com>
2025-06-26 08:14:10 +0700

    feat(topic-flashcard-v1.2): update flashcard practice page that use FlashcardPractice component

--- src/app/[locale]/flashcards/practice/page.tsx
+++ src/app/[locale]/flashcards/practice/page.tsx
@@ -1,44 +1,17 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import Flashcard from '../components/Flashcard';
-import { Angry, ArrowBigLeft, CircleAlert, Eye, Frown, Laugh, Smile, ThumbsUp } from 'lucide-react';
-import { Label } from '@/components/ui/label';
 import useFetch from '@/hooks/useFetch';
-import { Button } from '@/components/ui/button';
 import { putRequest } from '@/api/api';
-import BackButton from '../components/BackButton';
 import { IFlashcardFull, IQualityResponse, IQualityResponseNextReviewInterval } from '../flashcard.type';
-import { useTranslations } from 'next-intl';
 import LoadingPage from '@/app/loading';
-
-type TrackingOption = {
-    icon: any;
-    label: string;
-    qualityResponse: IQualityResponse;
-};
+import { FlashcardPractice } from '../practice/components/FlashcardPractice';
 
 export type IFlashcard = Pick<IFlashcardFull, 'flashcardId' | 'front' | 'back' | 'topicName'> & {
     qualityResponsesNextReviewInterval: IQualityResponseNextReviewInterval[];
 };
 
 export default function Page() {
-    const topicT = useTranslations('topic');
-    const flashcardT = useTranslations('flashcard.practice');
-
-    const trackingOptions: TrackingOption[] = [
-        { icon: <Angry size={24} fill="red" />, label: flashcardT('responseOptions.blackout'), qualityResponse: 0 },
-        { icon: <Frown size={24} fill="#F6C908" />, label: flashcardT('responseOptions.wrong'), qualityResponse: 1 },
-        {
-            icon: <CircleAlert size={24} fill="#FFCC4D" />,
-            label: flashcardT('responseOptions.wrongEasy'),
-            qualityResponse: 2,
-        },
-        { icon: <ThumbsUp size={24} fill="blue" />, label: flashcardT('responseOptions.hard'), qualityResponse: 3 },
-        { icon: <Smile size={24} fill="yellow" />, label: flashcardT('responseOptions.good'), qualityResponse: 4 },
-        { icon: <Laugh size={24} fill="yellow" />, label: flashcardT('responseOptions.perfect'), qualityResponse: 5 },
-    ];
-
     const {
         data: flashcards,
         setData: setFlashcardsData,
@@ -89,44 +62,7 @@ export default function Page() {
         }
     }
 
-    function renderTrackingOptionsSection(style: string) {
-        return (
-            <div className={style}>
-                {trackingOptions.map((option, index) => {
-                    const nextReviewInterval = currentFlashcard?.qualityResponsesNextReviewInterval.find(
-                        (element) => element.qualityResponse === option.qualityResponse,
-                    )?.nextReviewInterval;
-
-                    return (
-                        <div
-                            key={index}
-                            className="col-span-2 h-full flex flex-col gap-0 justify-center items-center rounded-lg bg-white dark:bg-gray-800 cursor-pointer"
-                            onClick={() => handleClickTrackingOption(option.qualityResponse)}
-                        >
-                            {option.icon}
-                            <Label className="text-lg">{option.label}</Label>
-                            <Label className="text-sm text-foreground">
-                                {nextReviewInterval} {nextReviewInterval === 1 ? 'day' : 'days'}
-                            </Label>
-                        </div>
-                    );
-                })}
-            </div>
-        );
-    }
-
-    function renderShowAnswerSection(style: string) {
-        return (
-            <div className={style}>
-                <Button onClick={handleManualFlip} className="flex flex-row items-center">
-                    <Eye />
-                    <div>{flashcardT('showAnswer')}</div>
-                </Button>
-            </div>
-        );
-    }
-
-    async function handleClickTrackingOption(qualityResponse: 0 | 1 | 2 | 3 | 4 | 5) {
+    async function handleOnClickTrackingOption(qualityResponse: IQualityResponse) {
         if (!flashcards || !currentFlashcard) return;
         try {
             await putRequest(`/flashcards/${currentFlashcard.flashcardId}/track`, { qualityResponse });
@@ -149,37 +85,17 @@ export default function Page() {
         return <div>Something went wrong with Flashcards</div>;
     }
 
-    // console.log('Flashcards: ', flashcards);
     return (
-        <div className="flex bg-gray-100 dark:bg-gray-950 h-[95vh]">
-            <div className="flex flex-1 flex-col m-1.25 mb-0 p-5">
-                <div className="bg-white dark:bg-gray-800 p-2.5">
-                    <BackButton />
-                </div>
-                <div className="flex-1 bg-gray-50 dark:bg-gray-900 p-5 grid grid-cols-11 gap-5">
-                    <div className="bg-gray-100 dark:bg-gray-950 col-span-11 flex flex-col items-center">
-                        <div className="w-[50%] flex flex-row justify-between mt-6">
-                            <div className="text-foreground">
-                                {topicT('topic')}
-                                <span> {currentFlashcard.topicName}</span>
-                            </div>
-                            <div className="text-foreground">
-                                <span>{flashcards.length} </span>
-                                {flashcardT('flashcardsRemaining')}
-                            </div>
-                        </div>
-                        <Flashcard
-                            style="flex w-[50%] h-[75%] mt-2"
-                            cardContainerRef={flashcardContainerRef}
-                            cardRef={cardRef}
-                            flashcard={currentFlashcard}
-                        />
-                        {shouldShowTrackingOptions
-                            ? renderTrackingOptionsSection('grid grid-cols-12 gap-6 mt-2 mt-4 mb-4 w-[70%] h-[20%]')
-                            : renderShowAnswerSection('mt-4')}
-                    </div>
-                </div>
-            </div>
-        </div>
+        <FlashcardPractice
+            topicName={currentFlashcard.topicName}
+            total={flashcards.length}
+            flashcardContainerRef={flashcardContainerRef}
+            cardRef={cardRef}
+            isFrontRef={isFrontRef}
+            flashcard={currentFlashcard}
+            handleManualFlip={handleManualFlip}
+            shouldShowTrackingOptions={shouldShowTrackingOptions}
+            handleOnClickTrackingOption={handleOnClickTrackingOption}
+        />
     );
 }
