d97a50ea0836fc3dc08161043a0ffbe3c035d7a9
vinhpm <cinepa.org@gmail.com>
2025-06-26 08:52:58 +0700

    fix(home): fix width header for issue #130

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -4,22 +4,22 @@ import React from 'react';
 import Navbar from '@/components/toolbar/Navbar';
 
 interface IHeaderProps {
-  isLoggedIn?: boolean;
-  username?: string;
-  className?: string;
+    isLoggedIn?: boolean;
+    username?: string;
+    className?: string;
 }
 
 const Header: React.FC<IHeaderProps> = ({ className = '' }) => {
-  return (
-    <header
-      className={`sticky top-0 z-40 w-full backdrop-blur ${className}`}
-      style={{
-        height: 'var(--header-height)',
-      }}
-    >
-      <Navbar />
-    </header>
-  );
+    return (
+        <header
+            className={`sticky top-0 z-[10000] w-full backdrop-blur ${className}`}
+            style={{
+                height: 'var(--header-height)',
+            }}
+        >
+            <Navbar />
+        </header>
+    );
 };
 
 export default Header;
