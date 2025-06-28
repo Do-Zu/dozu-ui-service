32c5a7b9314cc49b185eb1ec600a97b576ef91b8
KyAnh1126 <anh261104@gmail.com>
2025-06-28 16:00:42 +0700

    feat(topic-flashcard-v1.2): update TopicsList using TopicCard component

--- src/app/[locale]/topics/components/TopicsList.tsx
+++ src/app/[locale]/topics/components/TopicsList.tsx
@@ -10,6 +10,14 @@ import { ITopicForUser, ITopicsForUserReturned } from '../topic.type';
 import { TopicModal } from './TopicModal';
 import { useState } from 'react';
 import { TopicUpdatedForm } from './TopicUpdatedForm';
+import { useTranslations } from 'next-intl';
+import { TopicCard } from './TopicCard';
+
+function getDescriptionFormatted(description: string) {
+    let descriptionFormatted = description.slice(0, 50);
+    if (description.length > 50) descriptionFormatted += '...';
+    return descriptionFormatted;
+}
 
 interface Props {
     topics: ITopicsForUserReturned;
@@ -18,23 +26,9 @@ interface Props {
 
 const TopicsList = ({ topics, setTopics }: Props) => {
     const router = useRouter();
-
-    const [isTopicModalOpen, setIsTopicModalOpen] = useState<boolean>(false);
-    const [topicEditedName, setTopicEditedName] = useState<string>('');
-    const [topicEditedDescription, setTopicEditedDescription] = useState<string>('');
-
-    function handleOnClickStudy(topicId: number) {
-        router.push(`/flashcards/study?topicId=${topicId}`);
-    }
-
-    function handleOnClickEdit(name: string, description: string) {
-        setTopicEditedName(name);
-        setTopicEditedDescription(description);
-    }
-
-    function handleOnClickTitle(topicId: number) {
-        router.push(`flashcards/edit?topicId=${topicId}`);
-    }
+    
+    const [isTopicUpdatedModalOpen, setIsTopicUpdatedModalOpen] = useState<boolean>(false);
+    const [isTopicDeletedModalOpen, setIsTopicDeletedModalOpen] = useState<boolean>(false);
 
     function handleClickMindmap(topicId: number) {
         router.push(`mindmap/${topicId}`);
@@ -45,19 +39,14 @@ const TopicsList = ({ topics, setTopics }: Props) => {
             await deleteRequest(`/topics/${topicId}`);
             const topicsFiltered = topics!.filter((topic) => topic.topicId !== topicId);
             setTopics(topicsFiltered);
+            setIsTopicDeletedModalOpen(false);
         } catch (err) {
             console.log(err);
         }
     }
 
-    function getDescriptionFormatted(description: string) {
-        let descriptionFormatted = description.slice(0, 50);
-        if (description.length > 50) descriptionFormatted += '...';
-        return descriptionFormatted;
-    }
-
     // todo: check this function
-    const setTopicsCallback = (topic: ITopicForUser) => {
+    const updateTopics = (topic: ITopicForUser) => {
         if (topics === null) return;
         const topicsUpdated = topics.map((e) => {
             if (e.topicId === topic.topicId) return { ...e, name: topic.name, description: topic.description };
@@ -66,85 +55,23 @@ const TopicsList = ({ topics, setTopics }: Props) => {
         setTopics(topicsUpdated);
     };
 
-    function renderDeleteTopicAlertingDialog(topicId: number, topicName: string) {
-        const trigger = <Trash2 size={20} className="cursor-pointer" />;
-        return (
-            <DeleteAlertingModal
-                trigger={trigger}
-                title={`Delete topic ${topicName}`}
-                description={'This action will delete all items related to this topic. Still delete?'}
-                body={
-                    <div className="flex justify-end">
-                        <Button variant="destructive" onClick={() => handleOnClickDelete(topicId)}>
-                            Delete
-                        </Button>
-                    </div>
-                }
-            />
-        );
-    }
-
     return (
-        <div>
-            <div className="grid grid-cols-12 gap-8 flex-col mt-7">
-                {topics?.map((topic) => {
-                    return (
-                        <div className="col-span-3 bg-white rounded-lg p-4" key={topic.topicId}>
-                            <div className="flex flex-row items-center justify-between">
-                                <a
-                                    className="text-xl font-medium cursor-pointer hover:text-blue-600 hover:underline"
-                                    onClick={() => handleOnClickTitle(topic.topicId)}
-                                >
-                                    {topic.name}
-                                </a>
-                                <div className="flex flex-row gap-2">
-                                    <Book
-                                        size={20}
-                                        className="cursor-pointer"
-                                        onClick={() => handleOnClickStudy(topic.topicId)}
-                                    />{' '}
-                                    <Spline
-                                  
-                                        className="cursor-pointer"
-                                        onClick={() => handleClickMindmap(topic.topicId)}
-                                    />
-                                    <TopicModal
-                                        trigger={
-                                            <SquarePen
-                                                size={20}
-                                                className="cursor-pointer"
-                                                onClick={() => handleOnClickEdit(topic.name, topic.description)}
-                                            />
-                                        }
-                                        title="Edit Topic"
-                                        body={
-                                            <TopicUpdatedForm
-                                                topicId={topic.topicId}
-                                                name={topicEditedName}
-                                                description={topicEditedDescription}
-                                                setName={setTopicEditedName}
-                                                setDescription={setTopicEditedDescription}
-                                                handleCloseModal={() => setIsTopicModalOpen(false)}
-                                                setTopics={setTopicsCallback}
-                                            />
-                                        }
-                                        isOpen={isTopicModalOpen}
-                                        setIsOpen={setIsTopicModalOpen}
-                                    />
-                                    {renderDeleteTopicAlertingDialog(topic.topicId, topic.name)}
-                                </div>
-                            </div>
-                            <div className="text-muted-foreground text-base h-[80px]">
-                                {topic.description ? getDescriptionFormatted(topic.description) : 'No description'}
-                            </div>
-                            <div className="flex flex-row justify-between">
-                                <div className="text-muted-foreground text-sm">Last review: 14/05/2025</div>
-                                <div className="text-muted-foreground text-sm">{topic.flashcardsCount} Flashcards</div>
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {topics.map(topic => {
+                return (
+                    <TopicCard 
+                        key={topic.topicId}
+                        topic={topic}
+                        updateTopics={updateTopics}
+                        handleOnClickDelete={handleOnClickDelete}
+                        isTopicUpdatedModalOpen={isTopicUpdatedModalOpen}
+                        setIsTopicUpdatedModalOpen={setIsTopicUpdatedModalOpen}
+                        isTopicDeletedModalOpen={isTopicDeletedModalOpen}
+                        setIsTopicDeletedModalOpen={setIsTopicDeletedModalOpen}
+                    />
+                )
+                // return <ContentCard />
+            })}
         </div>
     );
 };
