ca4cdb34acb749ae15b0f210546cbdf96eeb2214
KyAnh1126 <anh261104@gmail.com>
2025-06-28 15:57:51 +0700

    feat(topic-flashcard-v1.2): add flashcardsDueToday field

--- src/app/[locale]/topics/topic.type.ts
+++ src/app/[locale]/topics/topic.type.ts
@@ -1,15 +1,15 @@
 export interface ITopic {
-    topicId: number
-    userId: number
-    name: string
-    description: string
-    createdAt: Date
-    imageUrl?: string
+    topicId: number;
+    userId: number;
+    name: string;
+    description: string;
+    createdAt: Date;
+    imageUrl?: string;
 }
 
 export type ITopicBasic = Pick<ITopic, 'topicId' | 'name' | 'description' | 'imageUrl'>;
 export type ITopicAdded = Pick<ITopic, 'userId' | 'name' | 'description'>;
 export type ITopicUpdated = Pick<ITopic, 'name' | 'description'>;
 
-export type ITopicForUser = ITopicBasic & { flashcardsCount? : number };
+export type ITopicForUser = ITopicBasic & { flashcardsCount?: number; flashcardsDueToday?: number };
 export type ITopicsForUserReturned = ITopicForUser[];
