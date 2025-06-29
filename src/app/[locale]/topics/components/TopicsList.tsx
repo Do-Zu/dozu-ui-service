351d08846a87f2209cb3c05b6b724e281c18eee9
KyAnh1126 <anh261104@gmail.com>
2025-06-29 17:33:20 +0700

    feat(topic-flashcard-v1.2): implement functions to pass to child component, add modal

--- src/app/[locale]/topics/components/TopicsList.tsx
+++ src/app/[locale]/topics/components/TopicsList.tsx
@@ -12,6 +12,7 @@ import { useState } from 'react';
 import { TopicUpdatedForm } from './TopicUpdatedForm';
 import { useTranslations } from 'next-intl';
 import { TopicCard } from './TopicCard';
+import { toast } from '@/hooks/use-toast';
 
 function getDescriptionFormatted(description: string) {
     let descriptionFormatted = description.slice(0, 50);
@@ -26,9 +27,19 @@ interface Props {
 
 const TopicsList = ({ topics, setTopics }: Props) => {
     const router = useRouter();
-    
+
+    const updatedFormT = useTranslations('topic.updatedForm');
+    const deletedFormT = useTranslations('topic.deletedForm');
+
     const [isTopicUpdatedModalOpen, setIsTopicUpdatedModalOpen] = useState<boolean>(false);
     const [isTopicDeletedModalOpen, setIsTopicDeletedModalOpen] = useState<boolean>(false);
+
+    const [topicEditedId, setTopicEditedId] = useState<number>(0);
+    const [topicEditedName, setTopicEditedName] = useState<string>('');
+    const [topicEditedDescription, setTopicEditedDescription] = useState<string>('');
+
+    const [topicDeletedId, setTopicDeletedId] = useState<number>(0);
+    const [topicDeletedName, setTopicDeletedName] = useState<string>('');
 
     function handleClickMindmap(topicId: number) {
         router.push(`mindmap/${topicId}`);
@@ -37,15 +48,21 @@ const TopicsList = ({ topics, setTopics }: Props) => {
     async function handleOnClickDelete(topicId: number) {
         try {
             await deleteRequest(`/topics/${topicId}`);
-            const topicsFiltered = topics!.filter((topic) => topic.topicId !== topicId);
-            setTopics(topicsFiltered);
+            deleteTopic(topicId);
             setIsTopicDeletedModalOpen(false);
+            toast({
+                title: 'Delete Topic successfully!',
+                variant: 'default'
+            })
         } catch (err) {
-            console.log(err);
+            toast({
+                title: 'Delete Topic failed, please try again',
+                variant: 'default'
+            })
         }
     }
 
-    // todo: check this function
+    // update topics state (UI) 
     const updateTopics = (topic: ITopicForUser) => {
         if (topics === null) return;
         const topicsUpdated = topics.map((e) => {
@@ -55,23 +72,74 @@ const TopicsList = ({ topics, setTopics }: Props) => {
         setTopics(topicsUpdated);
     };
 
+    // update topics state (UI)
+    const deleteTopic = (topicId: number) => {
+        const topicsFiltered = topics!.filter((topic) => topic.topicId !== topicId);
+        setTopics(topicsFiltered);
+    }
+
+    const onSelectEditTopic = ({ topicId, name, description } : { topicId: number, name: string, description: string }) => {
+        setTopicEditedId(topicId);
+        setTopicEditedName(name);
+        setTopicEditedDescription(description);
+
+        setTimeout(() => {
+            setIsTopicUpdatedModalOpen(true);
+        }, 50);
+    };
+
+    const onSelectDeleteTopic = ({ topicId, name } : { topicId: number, name: string }) => {
+        setTopicDeletedId(topicId);
+        setTopicDeletedName(name);
+
+        setTimeout(() => {
+            setIsTopicDeletedModalOpen(true);
+        }, 50);
+    }
+
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {topics.map(topic => {
+            {topics.map((topic) => {
                 return (
-                    <TopicCard 
+                    <TopicCard
                         key={topic.topicId}
                         topic={topic}
-                        updateTopics={updateTopics}
-                        handleOnClickDelete={handleOnClickDelete}
-                        isTopicUpdatedModalOpen={isTopicUpdatedModalOpen}
-                        setIsTopicUpdatedModalOpen={setIsTopicUpdatedModalOpen}
-                        isTopicDeletedModalOpen={isTopicDeletedModalOpen}
-                        setIsTopicDeletedModalOpen={setIsTopicDeletedModalOpen}
+                        onSelectEditTopic={onSelectEditTopic}
+                        onSelectDeleteTopic={onSelectDeleteTopic}
                     />
-                )
-                // return <ContentCard />
+                );
             })}
+
+            <TopicModal
+                title={updatedFormT('title')}
+                body={
+                    <TopicUpdatedForm
+                        topicId={topicEditedId}
+                        name={topicEditedName}
+                        description={topicEditedDescription}
+                        setName={setTopicEditedName}
+                        setDescription={setTopicEditedDescription}
+                        handleCloseModal={() => setIsTopicUpdatedModalOpen(false)}
+                        updateTopics={updateTopics}
+                    />
+                }
+                isOpen={isTopicUpdatedModalOpen}
+                setIsOpen={setIsTopicUpdatedModalOpen}
+            />
+
+            <DeleteAlertingModal
+                title={deletedFormT('title', { name: topicDeletedName })}
+                description={deletedFormT('description')}
+                body={
+                    <div className="flex justify-end">
+                        <Button variant="destructive" onClick={() => handleOnClickDelete(topicDeletedId)}>
+                            {deletedFormT('deleteButton')}
+                        </Button>
+                    </div>
+                }
+                isOpen={isTopicDeletedModalOpen}
+                setIsOpen={setIsTopicDeletedModalOpen}
+            />
         </div>
     );
 };
