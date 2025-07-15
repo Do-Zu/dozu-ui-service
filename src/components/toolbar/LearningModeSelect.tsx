2799a55354c74d36f63670e1e38d2ee1e27f7e85
KyAnh1126 <anh261104@gmail.com>
2025-07-15 18:46:31 +0700

    feat(class): update LearningModeSelect

--- src/components/toolbar/LearningModeSelect.tsx
+++ src/components/toolbar/LearningModeSelect.tsx
@@ -1,32 +1,62 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { useLocalStorage } from '@/hooks/useLocalStorage';
+import { ILearningMode, setLearningMode } from '@/stores/features/class-based-learning/learningModeSlice';
 import { CircleUserRound, School } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useRouter } from 'next/navigation';
+import { ROUTES } from '@/utils/constants/routes';
 
-type ILearningMode = 'personal' | 'class-based';
+function Personal() {
+    return (
+        <div className="flex items-center gap-2">
+            <CircleUserRound className="h-4 w-4" />
+            <span>Personal</span>
+        </div>
+    );
+}
+
+function ClassBased() {
+    return (
+        <div className="flex items-center gap-2">
+            <School className="h-4 w-4" />
+            <span>Class-based</span>
+        </div>
+    );
+}
 
 export function LearningModeSelect() {
-    const [learningMode, setLearningMode] = useState<ILearningMode>('personal');
+    const dispatch = useDispatch();
+    const [storedValue, setValue] = useLocalStorage<ILearningMode>('learningMode', 'personal');
+    const router = useRouter();
+
+    useEffect(() => {
+        dispatch(setLearningMode(storedValue));
+        if (storedValue === 'personal') {
+            router.push(ROUTES.HOME);
+        } else {
+            router.push(ROUTES.CLASS_BASED);
+        }
+    }, [storedValue]);
+
+    function handleLearningModelSelect(mode: ILearningMode) {
+        setValue(mode);
+    }
+
     return (
-        <Select >
-            <SelectTrigger className="w-[130px] bg-background text-foreground border-border" aria-label="Select theme">
-                <SelectValue placeholder="Select theme">
-                    <div className="flex items-center gap-2">
-                        
-                    </div>
-                </SelectValue>
+        <Select value={storedValue} onValueChange={handleLearningModelSelect}>
+            <SelectTrigger
+                className="ml-8 w-[150px] bg-background text-foreground border-border"
+                aria-label="Select theme"
+            >
+                <SelectValue>{storedValue === 'personal' ? <Personal /> : <ClassBased />}</SelectValue>
             </SelectTrigger>
             <SelectContent>
                 <SelectItem value="personal">
-                    <div className="flex items-center gap-2">
-                        <CircleUserRound className="h-4 w-4" />
-                        <span>Personal</span>
-                    </div>
+                    <Personal />
                 </SelectItem>
                 <SelectItem value="class-based">
-                    <div className="flex items-center gap-2">
-                        <School className="h-4 w-4" />
-                        <span>Class-based</span>
-                    </div>
+                    <ClassBased />
                 </SelectItem>
             </SelectContent>
         </Select>
