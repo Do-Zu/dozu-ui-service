76fee50df52fb5fd042261955af933c382387d9b
KyAnh1126 <anh261104@gmail.com>
2025-07-15 18:46:02 +0700

    feat(class): render different components for different roles or learning modes

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -4,16 +4,39 @@ import React from 'react';
 
 import CoreActionCards from './components/CoreActionCards';
 import CurrentProcessLearning from './components/CurrentProcessLearning';
-import { ClassesLibrary } from '../class-based-learning/components/ClassLibrary';
-import TopicLibrary from '../topics/components/TopicLibrary';
+import PersonalTopicLibrary from '../topics/components/personal/PersonalTopicLibrary';
+import { useSelector } from 'react-redux';
+import { selectLearningMode } from '@/stores/features/class-based-learning/learningModeSlice';
+import { StudentClassLibrary } from '../class-based/components/student/StudentClassLibrary';
+import { TeacherClassLibrary } from '../class-based/components/teacher/TeacherClassLibrary';
+import { useRoleChecker } from '@/hooks/useRoleChecker';
+import { useRouter } from 'next/navigation';
+import { ROUTES } from '@/utils/constants/routes';
 
 const Home: React.FC = () => {
+    const learningMode = useSelector(selectLearningMode);
+    const { isTeacher } = useRoleChecker();
+    const router = useRouter();
+
+    if(isTeacher) {
+        return (
+            <div className="flex flex-col h-full">
+                <CoreActionCards />
+                <CurrentProcessLearning />
+                <TeacherClassLibrary />
+            </div>
+        );
+    }
+
+    if(learningMode === 'class-based') {
+        router.push(ROUTES.CLASS_BASED);
+    }
+
     return (
         <div className="flex flex-col h-full">
             <CoreActionCards />
             <CurrentProcessLearning />
-            {/* <ClassesLibrary /> */}
-            <TopicLibrary />
+            <PersonalTopicLibrary />
         </div>
     );
 };
