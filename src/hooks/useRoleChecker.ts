3d48bf110f28962344d1852a664a420fda451ad0
KyAnh1126 <anh261104@gmail.com>
2025-07-24 15:25:32 +0700

    feat(class): edit isStudent

--- src/hooks/useRoleChecker.ts
+++ src/hooks/useRoleChecker.ts
@@ -3,7 +3,7 @@ import { useAuth } from '@/contexts/auth/AuthContext';
 export function useRoleChecker() {
     const { hasRole } = useAuth();
     return {
-        isStudent: hasRole('user') && !hasRole('teacher'),
+        isStudent: hasRole('user') && !hasRole('teacher') && !hasRole('admin'),
         isTeacher: hasRole('teacher'),
         isAdmin: hasRole('admin'),
     };
