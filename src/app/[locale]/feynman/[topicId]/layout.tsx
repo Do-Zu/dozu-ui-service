330c9b89a1b8c168ad9fef6e2c47aa9dcf1d94c7
vinhpm <phanminhvinh3010023@gmail.com>
2025-09-20 00:17:32 +0700

    chore(feynman): Remove unused import.

--- src/app/[locale]/feynman/[topicId]/layout.tsx
+++ src/app/[locale]/feynman/[topicId]/layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react';
-import DefaultLayout from '@/layouts/DefaultLayout';
 
 export default function FeynmanLayout({ children }: { children: ReactNode }) {
     return <div className="container mx-auto px-4 py-6 lg:py-8">{children}</div>;
