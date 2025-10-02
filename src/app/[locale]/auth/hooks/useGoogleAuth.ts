4a0d9d9d23e05dd01828e1aade85aa76bf150ea9
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2025-10-03 01:41:05 +0700

    hotfix(auth): correctly receive authState when using google login

--- src/app/[locale]/auth/hooks/useGoogleAuth.ts
+++ src/app/[locale]/auth/hooks/useGoogleAuth.ts
@@ -4,7 +4,7 @@ import { useSearchParams } from 'next/navigation';
 import Axios from '@/api/axios';
 import { useAuthNavigation } from '@/hooks/useAuthNavigation';
 import { useAuth } from '@/contexts/auth/AuthContext';
-import { User } from '@/types/auth';
+import { AuthState, User } from '@/types/auth';
 import { createGoogleAuthUrl } from '../login/utils/googleAuth';
 
 export const useGoogleAuth = () => {
@@ -52,10 +52,10 @@ export const useGoogleAuth = () => {
         loginWithGoogle();
     }, [code]);
 
-    const handleSuccessfulLogin = (user: User) => {
-        setAuthData(user);
+    const handleSuccessfulLogin = (authState: AuthState) => {
+        setAuthData(authState.user!);
 
-        handlePostLogin(user);
+        handlePostLogin(authState.user!);
     };
 
     return {
