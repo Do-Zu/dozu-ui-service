5e0c63850a371aca7c608814f76814ed202977e3
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2025-10-04 14:07:09 +0700

    fix(auth) register UI

--- src/app/[locale]/auth/register/page.tsx
+++ src/app/[locale]/auth/register/page.tsx
@@ -2,20 +2,22 @@
 
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import { useState } from 'react';
 import usePost from '@/hooks/usePost';
 
-import {  useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { toast } from '@/hooks/use-toast';
 import { withRouteGuard } from '@/components/guards/RouteGuard';
 import { useGoogleAuth } from '../hooks/useGoogleAuth';
 import { useAuth } from '@/contexts/auth/AuthContext';
 import { useAuthNavigation } from '@/hooks/useAuthNavigation';
 import { User } from '@/types/auth';
+import CentralCard from '@/components/auth/CentralCard';
+import { Mail, UserPlus } from 'lucide-react';
 
 const RegisterPage = () => {
     const { setAuthData } = useAuth();
@@ -65,7 +67,6 @@ const RegisterPage = () => {
 
             console.log(result.data);
             handleSuccessfulLogin(result.data);
-
         } catch (err) {
             console.error('Login error:', err);
         }
@@ -81,75 +82,82 @@ const RegisterPage = () => {
     //todo:check in again with the usePost types
 
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-muted px-4">
-            <div className="w-full max-w-md">
-                <Card className="rounded-2xl shadow-md">
-                    <CardContent className="p-6 space-y-6">
-                        <div className="text-center">
-                            <h1 className="text-2xl font-semibold">{t('title')}</h1>
-                            <p className="text-muted-foreground text-sm">{t('subtitle')}</p>
-                        </div>
+        <CentralCard>
+            <CardContent className="p-6 space-y-6">
+                <CardHeader className="text-center space-y-2">
+                    <CardTitle className="text-3xl font-bold">{t('title')}</CardTitle>
+                    <CardDescription className="text-base text-muted-foreground">{t('subtitle')}</CardDescription>
+                </CardHeader>
 
-                        <form onSubmit={handleSubmit} className="space-y-4">
-                            <div className="space-y-2">
-                                <Label htmlFor="username">{t('username')}</Label>
-                                <Input
-                                    id="username"
-                                    type="text"
-                                    placeholder={t('username')}
-                                    required
-                                    value={username}
-                                    onChange={(e) => {
-                                        setUsername(e.target.value);
-                                    }}
-                                />
-                            </div>
-                            <div className="space-y-2">
-                                <Label htmlFor="email">{t('email')}</Label>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    placeholder="you@example.com"
-                                    required
-                                    pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"
-                                    title="Please enter a valid email address"
-                                    value={email}
-                                    onChange={(e) => {
-                                        setEmail(e.target.value);
-                                    }}
-                                />
-                            </div>
-                            <div className="space-y-2">
-                                <Label htmlFor="password">{t('password')}</Label>
-                                <Input
-                                    id="password"
-                                    type="password"
-                                    placeholder="Create a password"
-                                    required
-                                    value={password}
-                                    onChange={(e) => {
-                                        setPassword(e.target.value);
-                                    }}
-                                />
-                            </div>
-                            <Button disabled={loading} className="w-full">
-                                {loading ? t('registerButtonTextLoading') : t('registerButtonText')}
-                            </Button>
-                            <Button disabled={loading} className="w-full" onClick={handleGoogleLogin}>
-                                {t('registerGoogleButtonText')}{' '}
-                            </Button>
-                        </form>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div className="space-y-2">
+                        <Label htmlFor="username">{t('username')}</Label>
+                        <Input
+                            id="username"
+                            type="text"
+                            placeholder={t('username')}
+                            required
+                            value={username}
+                            onChange={(e) => {
+                                setUsername(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="space-y-2">
+                        <Label htmlFor="email">{t('email')}</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            placeholder="you@example.com"
+                            required
+                            pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"
+                            title="Please enter a valid email address"
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="space-y-2">
+                        <Label htmlFor="password">{t('password')}</Label>
+                        <Input
+                            id="password"
+                            type="password"
+                            placeholder="Create a password"
+                            required
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}
+                        />
+                    </div>
 
-                        <div className="text-center text-sm text-muted-foreground">
-                            {t('haveAccountPrompt')}{' '}
-                            <Link href="/auth/login" className="underline">
-                                {t('loginButtonText')}
-                            </Link>
-                        </div>
-                    </CardContent>
-                </Card>
-            </div>
-        </div>
+                    <Button
+                        disabled={loading}
+                        className="w-full h-12 text-base font-medium flex items-center justify-center gap-2"
+                    >
+                        <UserPlus />
+                        {loading ? t('registerButtonTextLoading') : t('registerButtonText')}
+                    </Button>
+                    <Button
+                        variant="outline"
+                        disabled={loading}
+                        className="w-full h-12 text-base font-medium flex items-center justify-center gap-2"
+                        onClick={handleGoogleLogin}
+                    >
+                        <Mail className="h-5 w-5" />
+                        {t('registerGoogleButtonText')}
+                    </Button>
+                </form>
+
+                <div className="text-center text-sm text-muted-foreground">
+                    {t('haveAccountPrompt')}{' '}
+                    <Link href="/auth/login" className="underline">
+                        {t('loginButtonText')}
+                    </Link>
+                </div>
+            </CardContent>
+        </CentralCard>
     );
 };
 
