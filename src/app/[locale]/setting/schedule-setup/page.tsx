8dfd5ac370b6e8d53a18729f52ad9ef846b3fbb4
vinhpm <cinepa.org@gmail.com>
2025-10-11 15:31:27 +0700

    feat(schedule): apply multiple language for setting preference schedule page

--- src/app/[locale]/setting/schedule-setup/page.tsx
+++ src/app/[locale]/setting/schedule-setup/page.tsx
@@ -2,9 +2,11 @@
 
 import SetupForm from '@/app/[locale]/setting/schedule-setup/components/SetupForm';
 import { useRouter } from 'next/navigation';
+import { useTranslations } from 'next-intl';
 
 const ScheduleSetupPage = () => {
     const router = useRouter();
+    const t = useTranslations('schedule.setup');
 
     const handleComplete = () => {
         // You can add success notification here if needed
@@ -14,7 +16,7 @@ const ScheduleSetupPage = () => {
 
     return (
         <div className="max-w-full p-6 bg-white rounded-md shadow-sm overflow-auto">
-            <h1 className="text-xl font-semibold">Schedule Setup</h1>
+            <h1 className="text-xl font-semibold">{t('pageTitle')}</h1>
             <SetupForm onComplete={handleComplete} />
         </div>
     );
