f9ce4f1dd0a28d9fa544e649a45c27103962e216
vinhpm <cinepa.org@gmail.com>
2025-10-17 09:13:26 +0700

    fix(payment): Fix the incomplete hover class.

--- src/components/upgrade-plan/UpgradePlanModal.tsx
+++ src/components/upgrade-plan/UpgradePlanModal.tsx
@@ -80,7 +80,7 @@ export default function UpgradePlanModal() {
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
             <DialogContent className="sm:max-w-[480px] h-[95vh] max-h-[700px] overflow-y-hidden rounded-lg z-[1005]">
-                <DialogClose className="absolute right-4 top-4 z-10 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none text-gray-400 hover:">
+                <DialogClose className="absolute right-4 top-4 z-10 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none text-gray-400 hover:text-gray-600">
                     <X className="h-4 w-4" />
                     <span className="sr-only">Close</span>
                 </DialogClose>
