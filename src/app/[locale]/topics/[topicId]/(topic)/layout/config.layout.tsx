1dad70859b92459da86c75b25736d4804ab447ee
KyAnh1126 <anh261104@gmail.com>
2025-11-13 17:56:08 +0700

    feat(notewave): add NoteTab (again, due to code loss:)

--- src/app/[locale]/topics/[topicId]/(topic)/layout/config.layout.tsx
+++ src/app/[locale]/topics/[topicId]/(topic)/layout/config.layout.tsx
@@ -4,10 +4,12 @@ import MindMapTab from '../components/tabs/MindMapTab';
 import OverViewTab from '../components/tabs/OverViewTab';
 import QuizTab from '../components/tabs/QuizTab';
 import { TabConfig } from '../types';
+import NoteTab from '../components/tabs/NoteTab';
 
 export const TOPIC_WORKSPACE_TABS: TabConfig[] = [
     { value: 'overview', label: 'Overview', component: OverViewTab },
     { value: METHOD_LEARNING.MINDMAP, label: 'Mindmap', component: MindMapTab },
     { value: METHOD_LEARNING.FLASHCARD, label: 'Flashcards', component: FlashCardTab },
     { value: METHOD_LEARNING.QUIZ, label: 'Quiz', component: QuizTab },
+    { value: 'note', label: 'Note', component: NoteTab },
 ];
