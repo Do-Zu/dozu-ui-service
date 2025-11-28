05abc4684c7817baf3a992fe4f91352f87ea0e81
vinhpm <phanminhvinh3010023@gmail.com>
2025-11-28 16:43:35 +0700

    fix(core): Avoid applying both scrollbar-thin and scrollbar-hide to <html>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,11 +70,7 @@ const geist = localFont({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html
-            lang="en"
-            suppressHydrationWarning
-            className={`${inter.variable} ${geist.variable} scrollbar-thin scrollbar-hide`}
-        >
+        <html lang="en" suppressHydrationWarning className={`${inter.variable} ${geist.variable} scrollbar-hover`}>
             <body className="font-sans">
                 <StoreProvider>
                     <ThemeProvider>
