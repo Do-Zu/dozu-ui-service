e116072f095c71210ad70d00807464550db11a0d
vinhpm <phanminhvinh3010023@gmail.com>
2025-12-01 15:03:38 +0700

    refactor(reference): remove hard code type

---
+++ src/app/[locale]/topics/[topicId]/(topic)/types/reference.type.ts
@@ -0,0 +1,32 @@
+type TypeMetaDataChunkEmbed = {
+    type: string;
+    content: string | number | object | Array<unknown>;
+};
+
+type MetaDataYoutubeContent = { startTime: number };
+
+type MetaDataFileContent = {
+    pageNumber: number;
+};
+
+interface IReturnItemQuery {
+    embeddingId: number;
+    topicId: number;
+    contentType: string;
+    originContent: TypeMetaDataChunkEmbed;
+    metadata: MetaDataYoutubeContent | MetaDataFileContent | null;
+    createdAt: string | Date;
+    similarity: number;
+}
+
+interface IReturnItemFileReference {
+    embeddingId: number;
+    topicId: number;
+    contentType: string;
+    originContent: TypeMetaDataChunkEmbed;
+    metadata: MetaDataFileContent | null;
+    createdAt: string | Date;
+    similarity: number;
+}
+
+export type { MetaDataYoutubeContent, MetaDataFileContent, IReturnItemQuery, IReturnItemFileReference };
