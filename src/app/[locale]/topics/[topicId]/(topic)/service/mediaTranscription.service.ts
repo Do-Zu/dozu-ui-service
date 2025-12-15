e6f954dd731fb2d8b48fe19370afee5c68044bba
KyAnh1126 <anh261104@gmail.com>
2025-12-15 15:55:41 +0700

    feat(media-upload): change api endpoint

--- src/app/[locale]/topics/[topicId]/(topic)/service/mediaTranscription.service.ts
+++ src/app/[locale]/topics/[topicId]/(topic)/service/mediaTranscription.service.ts
@@ -5,7 +5,7 @@ class MediaTranscriptionService {
     public async getTranscripSegmentsFromMediaFile({ file }: { file: File }) {
         const formData = new FormData();
         formData.append('file', file);
-        const response = await postRequest<FormData, ITranscriptSegment[]>('/v1/audio-transcription', formData);
+        const response = await postRequest<FormData, ITranscriptSegment[]>('/v1/media-transcription', formData);
         if (response.status !== 'success') {
             throw new Error(response.message);
         }
